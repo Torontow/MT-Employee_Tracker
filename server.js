a0552d0e0d250404779d0e1ee78106da7acd70c0
Torontow <mtorontow0@gmail.com>
2021-01-06 11:59:35 -0500

    Built initial prompt
    Built the prompt that allows user to choose which aspect of the business they would like to manage.

--- server.js
+++ server.js
@@ -11,8 +11,59 @@ const connection = mysql.createConnection({
   database: 'emp_trackerDB',
 });
 
+// Function which prompts user for what they would like to manage--departments, employees, or employee roles.
+
+const start = () => {
+        inquirer
+            .prompt({
+                    name: 'manage',
+                    type: 'list',
+                    message: 'Hello! What would you like to manage today?',
+                    choices: [
+                        'Departments',
+                        'Employees',
+                        'Employee Roles',
+                    ],
+            })
+            .then((answer) => {
+                switch (answer.manage) {
+                    case 'Departments':
+                        console.log(answer.manage);
+                        break;
+                    case 'Employees':
+                        console.log(answer.manage);
+                        break;
+                    case 'Employee Roles':
+                        console.log(answer.manage);
+                        break;
+                    default:
+                        console.log(`Invalid action: ${answer.action}`);
+                }
+            });
+};
+// Function which prompts user for what they would like to do with departments.
+
+// Function which allows user to view all departments.
+
+// Function which allows user to add a department.
+
+// Function which prompts user for what they would like to do with employees.
+
+// Function which allows user to view all employees.
+
+// Function which allows user to add an employee.
+
+// Function which prompts user for what they would like to do with employee roles.
+
+// Function which allows user to view all employee roles.
+
+// Function which allows user to add an employee roles.
+
+
+
 // connect to the mysql server and sql database
 connection.connect((err) => {
     if (err) throw err;
     console.log("Connected.");
+    start();
 });  
