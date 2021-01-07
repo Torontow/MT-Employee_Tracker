29dfb342be2efe20c8884397f39a5a60ec06bb38
Torontow <mtorontow0@gmail.com>
2021-01-06 22:09:32 -0500

    Changed Employee Table Schema
    Made employee ID auto-increment.

--- schema.sql
+++ schema.sql
@@ -18,7 +18,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-    id INTEGER NOT NULL,
+    id INTEGER NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT NOT NULL,
